e819396a74edff53bef64fc892cb44ac4b60fafc
Sagar Dutta <duttasagar39@gmail.com>
2026-08-02 15:39:08 +0530

    Update SMTP and production settings

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -220,3 +220,5 @@ EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
+
+CSRF_TRUSTED_ORIGINS = os.getenv("CSRF_TRUSTED_ORIGINS", "").split(",")
