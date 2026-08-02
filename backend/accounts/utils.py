0f35ed479104efe69648e37efbe7a87c47c2b9c8
Sagar Dutta <duttasagar39@gmail.com>
2026-08-02 16:20:27 +0530

    Add SMTP debug logging

--- backend/accounts/utils.py
+++ backend/accounts/utils.py
@@ -3,12 +3,12 @@ from django.core.mail import send_mail
 import traceback
 
 def send_otp_email(email, otp):
-    try:
+    print("HOST:", settings.EMAIL_HOST, flush=True)
+    print("PORT:", settings.EMAIL_PORT, flush=True)
+    print("TLS:", settings.EMAIL_USE_TLS, flush=True)
+    print("USER:", settings.EMAIL_HOST_USER, flush=True)
 
-        print("HOST:", settings.EMAIL_HOST)
-        print("PORT:", settings.EMAIL_PORT)
-        print("TLS:", settings.EMAIL_USE_TLS)
-        print("USER:", settings.EMAIL_HOST_USER)
+    try:
         send_mail(
             subject="Email Verification OTP",
             message=f"Your OTP is {otp}",
@@ -17,27 +17,6 @@ def send_otp_email(email, otp):
             fail_silently=False,
         )
     except Exception as e:
-        print("EMAIL ERROR:", repr(e))
+        print("EMAIL ERROR:", repr(e), flush=True)
         traceback.print_exc()
         raise
-
-
-
-
-
-
-# from django.core.mail import send_mail
-# from django.conf import settings
-
-# def send_otp_email(email, otp):
-#     print("EMAIL HOST:", settings.EMAIL_HOST)
-#     print("EMAIL PORT:", settings.EMAIL_PORT)
-#     print("EMAIL USER:", settings.EMAIL_HOST_USER)
-#     print("EMAIL TLS:", settings.EMAIL_USE_TLS)
-#     send_mail(
-#         subject="Email Verification OTP",
-#         message=f"Your OTP is {otp}",
-#         from_email=settings.EMAIL_HOST_USER,
-#         recipient_list=[email],
-#         fail_silently=False,
-#     )
