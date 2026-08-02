ee05f399cade9c863f95b0c8a7143c1dc466fb95
Sagar Dutta <duttasagar39@gmail.com>
2026-08-02 14:39:44 +0530

    Add SMTP debug logs

--- backend/accounts/utils.py
+++ backend/accounts/utils.py
@@ -1,8 +1,11 @@
 from django.core.mail import send_mail
 from django.conf import settings
 
-
 def send_otp_email(email, otp):
+    print("EMAIL HOST:", settings.EMAIL_HOST)
+    print("EMAIL PORT:", settings.EMAIL_PORT)
+    print("EMAIL USER:", settings.EMAIL_HOST_USER)
+    print("EMAIL TLS:", settings.EMAIL_USE_TLS)
     send_mail(
         subject="Email Verification OTP",
         message=f"Your OTP is {otp}",
