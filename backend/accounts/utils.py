afff2993d8861a8a10303cb7701ba659d7109747
Sagar Dutta <duttasagar39@gmail.com>
2026-08-02 15:05:29 +0530

    Update email SMTP settings for production

--- backend/accounts/utils.py
+++ backend/accounts/utils.py
@@ -1,15 +1,34 @@
 from django.core.mail import send_mail
 from django.conf import settings
 
+
 def send_otp_email(email, otp):
-    print("EMAIL HOST:", settings.EMAIL_HOST)
-    print("EMAIL PORT:", settings.EMAIL_PORT)
-    print("EMAIL USER:", settings.EMAIL_HOST_USER)
-    print("EMAIL TLS:", settings.EMAIL_USE_TLS)
     send_mail(
         subject="Email Verification OTP",
         message=f"Your OTP is {otp}",
-        from_email=settings.EMAIL_HOST_USER,
+        from_email=settings.DEFAULT_FROM_EMAIL,
         recipient_list=[email],
         fail_silently=False,
     )
+
+
+
+
+
+
+
+# from django.core.mail import send_mail
+# from django.conf import settings
+
+# def send_otp_email(email, otp):
+#     print("EMAIL HOST:", settings.EMAIL_HOST)
+#     print("EMAIL PORT:", settings.EMAIL_PORT)
+#     print("EMAIL USER:", settings.EMAIL_HOST_USER)
+#     print("EMAIL TLS:", settings.EMAIL_USE_TLS)
+#     send_mail(
+#         subject="Email Verification OTP",
+#         message=f"Your OTP is {otp}",
+#         from_email=settings.EMAIL_HOST_USER,
+#         recipient_list=[email],
+#         fail_silently=False,
+#     )
